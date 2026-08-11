f2ee8c56a19e5b52e8cb24479544db9e2a28a350
Varun-Sharma-DY <varunsharmaus40@gmail.com>
2026-08-11 21:37:06 +0530

    app launch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,29 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.techno.roofing"
-    compileSdk = 35
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.messaging)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
